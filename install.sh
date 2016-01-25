30f1822782396894c1b95762cf9ede467fd39bd1
roel0 <roel.postelmans@altran.com>
2016-01-25 06:46:40 +0000

    git pull before install

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 printf '   vim_config by Roel0                \n'
 printf '   version 1.0                       \n'
+
+printf 'Updating vim configuration ... '
+git pull > /dev/null
+printf '[OK]\n'
 printf 'Installing vim configuration ... '
-
 cp -r ./.vim* ~/
-
 printf '[OK]\n'
