9adb046a675043f5f613c84502d74fb8523b06cd
roel0 <roel.postelmans@altran.com>
2016-01-25 06:45:02 +0000

    Added installation script

---
+++ install.sh
@@ -0,0 +1,7 @@
+printf '   vim_config by Roel0                \n'
+printf '   version 1.0                       \n'
+printf 'Installing vim configuration ... '
+
+cp -r ./.vim* ~/
+
+printf '[OK]\n'
